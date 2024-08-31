c2a0a4df6c6a78a17c5a7dfa668e50d84f6aff81
darsh12shah <darsh12shah@gmail.com>
2024-08-31 19:32:19 +0530

    deploy ignore

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   images: {
     domains: ["localhost", "https://img.clerk.com", "img.clerk.com"],
+  }, typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 };
 
